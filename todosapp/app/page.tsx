2da85e0b91b3adcf643a4d615c3ca32817304745
djamet <kampretsiji1@gmail.com>
2024-01-26 13:49:48 +0700

    changes

--- todosapp/app/page.tsx
+++ todosapp/app/page.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function HomeComponent() {
   return (
-    <div className=" w-screen h-screen bg-black text-white font-figtree flex flex-col justify-center items-center">
+    <div className=" w-screen h-screen bg-black text-white font-figtree flex flex-col gap-2 justify-center items-center">
       <h1 className=" text-xl">Welcome to todos app</h1>
       <div>
-        <Link href={"/auth/login"} className=" border p-1 rounded-lg">
+        <Link href={"/auth/login"} className=" border p-1 rounded-xl">
           Login
         </Link>
       </div>
